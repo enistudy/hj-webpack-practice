0c0325e340b766718c1515b0a9605ec0a77b29bc
Logqwerty <meg0070@naver.com>
2020-01-08 21:43:40 +0900

    [Feat] @babel/polyfill이 필요한 예제 코드 추가.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 import log from "./log";
 
 log("Hello, Webpack");
+
+Promise.resolve(1).then(log);
+
+log(JSON.stringify(Object.assign({}, { a: 1 }, { b: 2 }), null, 2));
+
+log(Array.from(Array(5), _ => 0));
