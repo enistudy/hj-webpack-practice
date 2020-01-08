c997b66ca99ad3567089f66bfe002fc209d3970a
Logqwerty <meg0070@naver.com>
2020-01-08 21:22:39 +0900

    [Feat] webpack 설정에 babel-loader 추가
    js파일을 트랜스 컴파일하는 babel를 동작시키기 위해 webpack 설정에
    babel-loader관련 설정 추가.

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,17 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
