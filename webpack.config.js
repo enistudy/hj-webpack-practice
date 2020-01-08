4d46e386fc2c4769dd55b54b92fed05dba6bb969
Logqwerty <meg0070@naver.com>
2020-01-09 02:22:15 +0900

    [Feat] ts-loader 관련 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/js/index.js", "./src/sass/style.scss"],
+  entry: ["@babel/polyfill", "./src/js/index.js", "./src/ts/index.ts", "./src/sass/style.scss"],
   output: {
     filename: "bundle.js",
     publicPath: "dist",
     path: path.join(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
   module: {
@@ -30,6 +33,11 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
       },
     ],
   },
